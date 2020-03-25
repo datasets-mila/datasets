3cb0c0b47a5fc4177f561b722f786ff5daf251a0
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2020-03-25 12:27:23 -0400

    Fix aggregate_metadata.py and optimize parsing
    Prevent parsing the main dataset when the commit is has not changed

--- scripts/aggregate_metadata.py
+++ scripts/aggregate_metadata.py
@@ -53,6 +53,16 @@ def ds_need_update(root_path, ds_path):
     return ds_json.get("commit_hash", None) != git.Repo(abs_path).head.object.hexsha
 
 
+def filter_nodes_duplicates(nodes):
+    included_names = set()
+    filtered_nodes = []
+    for node in nodes:
+        if node["name"] not in included_names:
+            filtered_nodes.append(node)
+            included_names.add(node["name"])
+    return filtered_nodes
+
+
 ds = Dataset(".")
 
 subds_var = {}
@@ -65,13 +75,16 @@ for subds in ds.subdatasets():
     subds_var[path].sort()
     subds_path.extend(subds_var[path])
 
+
+is_ds_need_update = ds_need_update(ds.path, '.')
+ds_to_update = [path for path in subds_path if ds_need_update(ds.path, path)]
+
 push_git_dir(os.path.join(ds.path, ".git"))
-ds.aggregate_metadata()
-
-ls([path for path in subds_path if ds_need_update(ds.path, path)],
-        recursive=True, all_=True, long_=True, json="file")
-
-ls('.', recursive=False, all_=True, long_=True, json="file")
+if is_ds_need_update:
+    ds.aggregate_metadata()
+ls(ds_to_update, recursive=True, all_=True, long_=True, json="file")
+if is_ds_need_update:
+    ls('.', recursive=False, all_=True, long_=True, json="file")
 pop_git_dir()
 
 for path in ['.'] + subds_path:
@@ -84,7 +97,7 @@ for path in ['.'] + subds_path:
     dump_json(abs_path, '/', ds_json)
 
 ds_json = load_json(ds.path, '/');
-ds_json["nodes"] = [node for node in ds_json["nodes"] if len(node["path"]) == 1 or node["path"][0] != "."]
+ds_json["nodes"] = [node for node in ds_json["nodes"] if len(node["name"]) == 1 or node["name"][0] != "."]
 dump_json(ds.path, '/', ds_json)
 
 del ds_json["nodes"]
@@ -101,9 +114,11 @@ for subds_var_dir, subds_vars_path in subds_var.items():
         ds_json["name"] = ".."
         ds_json["path"] = os.path.relpath(".", subds_var_path)
         var_json["nodes"].insert(1, copy.deepcopy(ds_json))
+        var_json["nodes"] = filter_nodes_duplicates(var_json["nodes"])
         dump_json(os.path.join(ds.path, subds_var_path), '/', var_json)
 
         del var_json["nodes"]
         dir_json["nodes"].append(var_json)
 
+    dir_json["nodes"] = filter_nodes_duplicates(dir_json["nodes"])
     dump_json(ds.path, subds_var_dir, dir_json)
