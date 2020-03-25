58878f509d450884b5e7d2c582cbbe2f6d2ce2c8
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2020-03-25 11:57:27 -0400

    Avoid recursive gathering of files when not needed
    Use commit hash to check if a dataset has content changes

--- scripts/aggregate_metadata.py
+++ scripts/aggregate_metadata.py
@@ -6,57 +6,104 @@ import json
 import os
 
 from datalad.api import Dataset, ls
+import git
 
 
 METADATA_PATH = ".git/datalad/metadata"
+_GIT_DIR_STACK = []
+
+
+def push_git_dir(path):
+    try:
+        _GIT_DIR_STACK.append(os.environ["GIT_DIR"])
+    except KeyError:
+        pass
+
+    os.environ["GIT_DIR"] = path
+
+
+def pop_git_dir():
+    try:
+        os.environ["GIT_DIR"] = _GIT_DIR_STACK.pop()
+    except IndexError:
+        del os.environ["GIT_DIR"]
+
+
+def load_json(ds_path, entry):
+    path = os.path.join(ds_path, METADATA_PATH,
+            hashlib.md5(entry.encode("utf-8")).hexdigest())
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
+def dump_json(ds_path, entry, json_content):
+    path = os.path.join(ds_path, METADATA_PATH,
+            hashlib.md5(entry.encode("utf-8")).hexdigest())
+    with open(path, 'w') as f:
+        json.dump(json_content, f)
+
+
+def ds_need_update(root_path, ds_path):
+    abs_path = os.path.join(root_path, ds_path)
+    try:
+        ds_json = load_json(abs_path, '/')
+    except FileNotFoundError as error:
+        return True
+
+    return ds_json.get("commit_hash", None) != git.Repo(abs_path).head.object.hexsha
+
 
 ds = Dataset(".")
 
 subds_var = {}
+subds_path = []
 for subds in ds.subdatasets():
-    path = subds["path"][len(ds.path.rstrip('/'))+1:].rstrip('/') + ".var"
+    path = subds["path"][len(ds.path.rstrip('/'))+1:].rstrip('/')
+    subds_path.append(path)
+    path += ".var"
     subds_var[path] = glob.glob(os.path.join(path, "*"))
     subds_var[path].sort()
+    subds_path.extend(subds_var[path])
 
+push_git_dir(os.path.join(ds.path, ".git"))
 ds.aggregate_metadata()
 
-ls(["."] + [path for var_paths in subds_var.values() for path in var_paths],
+ls([path for path in subds_path if ds_need_update(ds.path, path)],
         recursive=True, all_=True, long_=True, json="file")
 
-ds_path = os.path.join(ds.path, METADATA_PATH,
-        hashlib.md5('/'.encode("utf-8")).hexdigest())
-with open(ds_path, 'r') as f:
-    ds_json = json.load(f)
+ls('.', recursive=False, all_=True, long_=True, json="file")
+pop_git_dir()
 
+for path in ['.'] + subds_path:
+    abs_path = os.path.join(ds.path, path)
+    try:
+        ds_json = load_json(abs_path, '/')
+    except FileNotFoundError as error:
+        continue
+    ds_json["commit_hash"] = git.Repo(abs_path).head.object.hexsha
+    dump_json(abs_path, '/', ds_json)
+
+ds_json = load_json(ds.path, '/');
 ds_json["nodes"] = [node for node in ds_json["nodes"] if len(node["path"]) == 1 or node["path"][0] != "."]
-with open(ds_path, 'w') as f:
-    json.dump(ds_json, f)
+dump_json(ds.path, '/', ds_json)
 
 del ds_json["nodes"]
 
-for subds_var_root, subds_var_paths in subds_var.items():
+for subds_var_dir, subds_vars_path in subds_var.items():
     try:
-        root_path = os.path.join(ds.path, METADATA_PATH,
-                hashlib.md5(subds_var_root.encode("utf-8")).hexdigest())
-        with open(root_path, 'r') as f:
-            root_json = json.load(f)
+        dir_json = load_json(ds.path, subds_var_dir);
     except FileNotFoundError as error:
         continue
 
-    for subds_var_path in subds_var_paths:
-        var_path = os.path.join(ds.path, subds_var_path, METADATA_PATH,
-                hashlib.md5('/'.encode("utf-8")).hexdigest())
-        with open(var_path, 'r') as f:
-            var_json = json.load(f)
+    for subds_var_path in subds_vars_path:
+        var_json = load_json(os.path.join(ds.path, subds_var_path), '/');
 
         ds_json["name"] = ".."
         ds_json["path"] = os.path.relpath(".", subds_var_path)
         var_json["nodes"].insert(1, copy.deepcopy(ds_json))
-        with open(var_path, 'w') as f:
-            json.dump(var_json, f)
+        dump_json(os.path.join(ds.path, subds_var_path), '/', var_json)
 
         del var_json["nodes"]
-        root_json["nodes"].append(var_json)
+        dir_json["nodes"].append(var_json)
 
-    with open(root_path, 'w') as f:
-        json.dump(root_json, f)
+    dump_json(ds.path, subds_var_dir, dir_json)
