3038fb156239a27a09600708e2b9efce19fe5433
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2022-06-20 13:54:14 -0400

    Sync all datasets to all cc clusters

--- scripts/sync_all_datasets.sh
+++ scripts/sync_all_datasets.sh
@@ -8,9 +8,19 @@ source scripts/activate_datalad.sh
 source scripts/utils.sh echo -n
 set -o errexit -o pipefail
 
+_cc_clusters=(mila cc-beluga cc-narval cc-cedar)
 subdatasets --var | while read subds
 do
-	./scripts/sync_dataset.sh --dataset "${subds}"
+	./scripts/sync_dataset.sh --dataset "${subds}" --from "${_cc_clusters[0]}" --to "${_cc_clusters[1]}"
+	for _cluster in "${_cc_clusters[@]:2}"
+	do
+		./scripts/sync_dataset.sh --dataset "${subds}" --from "${_cc_clusters[1]}" --to "${_cluster}" &
+	done
+
+	for _jid in $(jobs -rp)
+	do
+		wait $_jid
+	done
 done
 
 popd
