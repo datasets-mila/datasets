74ac3381edf0ec92b9d674db9605ff8f797b1bcb
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2022-12-21 17:41:40 -0500

    Push list_datasets_cc.sh to remote clusters

--- scripts/sync_all_datasets.sh
+++ scripts/sync_all_datasets.sh
@@ -11,6 +11,36 @@ set -o errexit -o pipefail
 WL_DIR=.$(basename $PWD)_sync_tree/.whitelist/
 
 _cc_clusters=(mila cc-beluga cc-narval cc-cedar)
+
+# Copy scripts/list_datasets_cc.sh to remote clusters
+for _cluster in "${_cc_clusters[@]:1}"
+do
+	_prefix=$(echo "$_cluster" | cut -d'-' -f1 -)
+	_cluster=$(echo "$_cluster" | cut -d'-' -f2 -)
+
+	if [[ -z "${_cluster}" ]]
+	then
+		_cluster=${_prefix}
+	fi
+
+	if [[ "${_prefix}" == "cc" ]]
+	then
+		_config_cluster=computecanada.${_cluster}
+	fi
+
+	echo "$_prefix"
+	echo "$_cluster"
+	echo "$_config_cluster"
+	echo "_TO_ROOT=\$(git config -f scripts/config/config --get ${_config_cluster}.data-root)"
+
+	_TO_ROOT=$(git config -f scripts/config/config --get ${_config_cluster}.data-root)
+
+	echo $_TO_ROOT
+
+	# cc-cedar has issues connecting so this might fail here
+	scp scripts/list_datasets_cc.sh ${_cluster}:"${_TO_ROOT}/list_datasets_cc.sh"
+done
+
 subdatasets --var | while read subds
 do
 	if ! grep "^1" "${WL_DIR}/${subds}/check" > /dev/null
